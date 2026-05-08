78c428f147650e8389e1ba26baa9ee7c94e73528
jymer1102 <jacksonaweimer@icloud.com>
2026-05-08 09:52:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("Backend is running!"));
+
 app.post("/chat", async (req, res) => {
   const { messages } = req.body;
 
