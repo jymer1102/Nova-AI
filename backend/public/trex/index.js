b02896c740c6c9bc39087506a08bbd26fd5ae500
jymer1102 <jacksonaweimer@icloud.com>
2026-05-09 22:03:17 -0500

    Update index.js

--- backend/public/trex/index.js
+++ backend/public/trex/index.js
@@ -1,279 +1,87 @@
-// T-Rex Runner logic
 (function() {
-  'use strict';
-  function Runner(containerSelector, opt_config) {
-    if (Runner.instance_) { return Runner.instance_; }
-    Runner.instance_ = this;
-    this.outerContainerEl = document.querySelector(containerSelector);
-    this.containerEl = null;
-    this.config = opt_config || Runner.config;
-    this.dimensions = Runner.defaultDimensions;
-    this.canvas = null;
-    this.ctx = null;
-    this.tRex = null;
-    this.distanceMeter = null;
-    this.distanceRan = 0;
-    this.highestScore = 0;
-    this.time = 0;
-    this.runningTime = 0;
-    this.msPerFrame = 1000 / 60;
-    this.currentSpeed = this.config.SPEED;
-    this.obstacles = [];
-    this.activated = false; 
-    this.playing = false;
-    this.crashed = false;
-    this.paused = false;
-    this.images = {};
-    this.imagesLoaded = 0;
-    this.loadImages();
-  }
-  window['Runner'] = Runner;
-
-  Runner.config = {
-    ACCELERATION: 0.001,
-    BG_CLOUD_SPEED: 0.2,
-    BOTTOM_PAD: 10,
-    CLEAR_TIME: 3000,
-    CLOUD_FREQUENCY: 0.5,
-    GAMEOVER_CLEAR_TIME: 750,
-    GAP_COEFFICIENT: 0.6,
-    GRAVITY: 0.6,
-    INITIAL_JUMP_VELOCITY: 12,
-    MAX_CLOUDS: 6,
-    MAX_OBSTACLE_LENGTH: 3,
-    MAX_SPEED: 13,
-    MIN_JUMP_HEIGHT: 35,
-    MOBILE_SPEED_COEFFICIENT: 1.2,
-    RESOURCE_TEMPLATE_ID: 'offline-resources',
-    SPEED: 6,
-    SPEED_DROP_COEFFICIENT: 3
-  };
-
-  Runner.defaultDimensions = {
-    WIDTH: 600,
-    HEIGHT: 150
-  };
-
-  Runner.prototype = {
-    loadImages: function() {
-      var spriteSheet = document.getElementById('offline-resources-1x');
-      if (spriteSheet.complete) {
+    'use strict';
+    // --- CORE RUNNER ---
+    function Runner(containerSelector) {
+        this.outerContainerEl = document.querySelector(containerSelector);
+        this.config = { SPEED: 6, GRAVITY: 0.6, SPAWN_RATE: 0.02 };
+        this.dimensions = { WIDTH: 600, HEIGHT: 150 };
+        this.canvas = null;
+        this.ctx = null;
+        this.tRex = { x: 50, y: 100, v: 0, jumping: false };
+        this.obstacles = [];
+        this.playing = false;
+        this.score = 0;
+        this.sprite = document.getElementById('offline-resources-1x');
         this.init();
-      } else {
-        spriteSheet.onload = this.init.bind(this);
-      }
-    },
-    init: function() {
-      this.containerEl = document.createElement('div');
-      this.containerEl.className = 'runner-container';
-      this.canvas = document.createElement('canvas');
-      this.canvas.className = 'runner-canvas';
-      this.canvas.width = this.dimensions.WIDTH;
-      this.canvas.height = this.dimensions.HEIGHT;
-      this.containerEl.appendChild(this.canvas);
-      this.outerContainerEl.appendChild(this.containerEl);
-      this.ctx = this.canvas.getContext('2d');
-      this.ctx.fillStyle = '#f7f7f7';
-      this.ctx.fill();
-      this.update();
-      this.addEventListeners();
-    },
-    addEventListeners: function() {
-      document.addEventListener('keydown', this.onKeyDown.bind(this));
-      document.addEventListener('keyup', this.onKeyUp.bind(this));
-    },
-    onKeyDown: function(e) {
-      if (!this.playing) {
-        this.playing = true;
-        this.update();
-      }
-    },
-    onKeyUp: function(e) {},
-    update: function() {
-      this.ctx.clearRect(0, 0, this.dimensions.WIDTH, this.dimensions.HEIGHT);
-      // Drawing logic goes here - simplified for this example
-      if (this.playing) {
-        this.runningTime += this.msPerFrame;
-        window.requestAnimationFrame(this.update.bind(this));
-      } else {
-         this.ctx.fillText('Press any key to start', 250, 75);
-      }
     }
-  };
-})();
 
-// Start the game
-new Runner('.interstitial-wrapper');
-         * @param {number} deltaTime
-         * @param {number} currentSpeed
-         * @param {boolean} updateObstacles Used as an override to prevent
-         *     the obstacles from being updated / added. This happens in the
-         *     ease in section.
-         * @param {boolean} showNightMode Night mode activated.
-         */
-        update: function (deltaTime, currentSpeed, updateObstacles, showNightMode) {
-            this.runningTime += deltaTime;
-            this.horizonLine.update(deltaTime, currentSpeed);
-            this.nightMode.update(showNightMode);
-            this.updateClouds(deltaTime, currentSpeed);
-
-            if (updateObstacles) {
-                this.updateObstacles(deltaTime, currentSpeed);
-            }
-        },
-
-        /**
-         * Update the cloud positions.
-         * @param {number} deltaTime
-         * @param {number} currentSpeed
-         */
-        updateClouds: function (deltaTime, speed) {
-            var cloudSpeed = this.cloudSpeed / 1000 * deltaTime * speed;
-            var numClouds = this.clouds.length;
-
-            if (numClouds) {
-                for (var i = numClouds - 1; i >= 0; i--) {
-                    this.clouds[i].update(cloudSpeed);
-                }
-
-                var lastCloud = this.clouds[numClouds - 1];
-
-                // Check for adding a new cloud.
-                if (numClouds < this.config.MAX_CLOUDS &&
-                    (this.dimensions.WIDTH - lastCloud.xPos) > lastCloud.cloudGap &&
-                    this.cloudFrequency > Math.random()) {
-                    this.addCloud();
-                }
-
-                // Remove expired clouds.
-                this.clouds = this.clouds.filter(function (obj) {
-                    return !obj.remove;
-                });
-            } else {
-                this.addCloud();
-            }
-        },
-
-        /**
-         * Update the obstacle positions.
-         * @param {number} deltaTime
-         * @param {number} currentSpeed
-         */
-        updateObstacles: function (deltaTime, currentSpeed) {
-            // Obstacles, move to Horizon layer.
-            var updatedObstacles = this.obstacles.slice(0);
-
-            for (var i = 0; i < this.obstacles.length; i++) {
-                var obstacle = this.obstacles[i];
-                obstacle.update(deltaTime, currentSpeed);
-
-                // Clean up existing obstacles.
-                if (obstacle.remove) {
-                    updatedObstacles.shift();
+    Runner.prototype.init = function() {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.dimensions.WIDTH;
+        this.canvas.height = this.dimensions.HEIGHT;
+        this.outerContainerEl.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+        
+        window.addEventListener('keydown', (e) => {
+            if (e.code === 'Space') {
+                if (!this.playing) this.playing = true;
+                if (!this.tRex.jumping) {
+                    this.tRex.v = -10;
+                    this.tRex.jumping = true;
                 }
             }
-            this.obstacles = updatedObstacles;
-
-            if (this.obstacles.length > 0) {
-                var lastObstacle = this.obstacles[this.obstacles.length - 1];
-
-                if (lastObstacle && !lastObstacle.followingObstacleCreated &&
-                    lastObstacle.isVisible() &&
-                    (lastObstacle.xPos + lastObstacle.width + lastObstacle.gap) <
-                    this.dimensions.WIDTH) {
-                    this.addNewObstacle(currentSpeed);
-                    lastObstacle.followingObstacleCreated = true;
-                }
-            } else {
-                // Create new obstacles.
-                this.addNewObstacle(currentSpeed);
-            }
-        },
-
-        removeFirstObstacle: function () {
-            this.obstacles.shift();
-        },
-
-        /**
-         * Add a new obstacle.
-         * @param {number} currentSpeed
-         */
-        addNewObstacle: function (currentSpeed) {
-            var obstacleTypeIndex = getRandomNum(0, Obstacle.types.length - 1);
-            var obstacleType = Obstacle.types[obstacleTypeIndex];
-
-            // Check for multiples of the same type of obstacle.
-            // Also check obstacle is available at current speed.
-            if (this.duplicateObstacleCheck(obstacleType.type) ||
-                currentSpeed < obstacleType.minSpeed) {
-                this.addNewObstacle(currentSpeed);
-            } else {
-                var obstacleSpritePos = this.spritePos[obstacleType.type];
-
-                this.obstacles.push(new Obstacle(this.canvasCtx, obstacleType,
-                    obstacleSpritePos, this.dimensions,
-                    this.gapCoefficient, currentSpeed, obstacleType.width));
-
-                this.obstacleHistory.unshift(obstacleType.type);
-
-                if (this.obstacleHistory.length > 1) {
-                    this.obstacleHistory.splice(Runner.config.MAX_OBSTACLE_DUPLICATION);
-                }
-            }
-        },
-
-        /**
-         * Returns whether the previous two obstacles are the same as the next one.
-         * Maximum duplication is set in config value MAX_OBSTACLE_DUPLICATION.
-         * @return {boolean}
-         */
-        duplicateObstacleCheck: function (nextObstacleType) {
-            var duplicateCount = 0;
-
-            for (var i = 0; i < this.obstacleHistory.length; i++) {
-                duplicateCount = this.obstacleHistory[i] == nextObstacleType ?
-                    duplicateCount + 1 : 0;
-            }
-            return duplicateCount >= Runner.config.MAX_OBSTACLE_DUPLICATION;
-        },
-
-        /**
-         * Reset the horizon layer.
-         * Remove existing obstacles and reposition the horizon line.
-         */
-        reset: function () {
-            this.obstacles = [];
-            this.horizonLine.reset();
-            this.nightMode.reset();
-        },
-
-        /**
-         * Update the canvas width and scaling.
-         * @param {number} width Canvas width.
-         * @param {number} height Canvas height.
-         */
-        resize: function (width, height) {
-            this.canvas.width = width;
-            this.canvas.height = height;
-        },
-
-        /**
-         * Add a new cloud to the horizon.
-         */
-        addCloud: function () {
-            this.clouds.push(new Cloud(this.canvas, this.spritePos.CLOUD,
-                this.dimensions.WIDTH));
-        }
+        });
+        this.update();
     };
+
+    Runner.prototype.update = function() {
+        this.ctx.clearRect(0, 0, this.dimensions.WIDTH, this.dimensions.HEIGHT);
+        
+        if (this.playing) {
+            // Physics
+            this.tRex.y += this.tRex.v;
+            this.tRex.v += this.config.GRAVITY;
+            if (this.tRex.y > 100) {
+                this.tRex.y = 100;
+                this.tRex.jumping = false;
+            }
+
+            // Spawn Obstacles
+            if (Math.random() < this.config.SPAWN_RATE) {
+                this.obstacles.push({ x: 600, w: 20, h: 40 });
+            }
+
+            // Move Obstacles
+            for (let i = this.obstacles.length - 1; i >= 0; i--) {
+                this.obstacles[i].x -= this.config.SPEED;
+                // Collision check
+                if (this.obstacles[i].x < this.tRex.x + 20 && this.obstacles[i].x + 20 > this.tRex.x && this.tRex.y > 80) {
+                   this.playing = false; // Game Over
+                }
+                if (this.obstacles[i].x < -20) this.obstacles.splice(i, 1);
+            }
+            this.score++;
+        }
+
+        // DRAWING
+        this.ctx.fillStyle = '#535353';
+        // Draw T-Rex (Using sprite if loaded, else a box)
+        if (this.sprite.complete) {
+            this.ctx.drawImage(this.sprite, 848, 2, 44, 47, this.tRex.x, this.tRex.y, 44, 47);
+        } else {
+            this.ctx.fillRect(this.tRex.x, this.tRex.y, 40, 40);
+        }
+        
+        // Draw Obstacles
+        for (let obs of this.obstacles) {
+            this.ctx.fillRect(obs.x, 110, 20, 30);
+        }
+
+        this.ctx.fillText("Score: " + this.score, 500, 20);
+        if (!this.playing) this.ctx.fillText("PRESS SPACE TO START", 250, 75);
+
+        window.requestAnimationFrame(this.update.bind(this));
+    };
+
+    document.addEventListener('DOMContentLoaded', () => new Runner('.interstitial-wrapper'));
 })();
-
-
-function onDocumentLoad() {
-    new Runner('.interstitial-wrapper');
-}
-
-document.addEventListener('DOMContentLoaded', onDocumentLoad);
-// Start the game
-document.addEventListener('DOMContentLoaded', function() {
-  new Runner('.interstitial-wrapper');
-});
