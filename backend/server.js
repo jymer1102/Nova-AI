4246fddd497d7d55ca00159e2e98c650d4c5ed29
Jackson Weimer <jacksonaweimer@icloud.com>
2026-07-20 16:05:27 -0500

    Change static files directory to jekyll_build

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "jekyll_build")));
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
